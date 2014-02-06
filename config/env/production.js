131dea194e945a4c8d9bbbb443be348d6f56d18c
Tyler Wolf <tylernicholaswolf@gmail.com>
2014-02-05 23:17:51 -0500

    updated mongo db

--- config/env/production.js
+++ config/env/production.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-    db: "mongodb://localhost/mean",
+    db: "mongodb://<dbuser>:<dbpassword>@ds027489.mongolab.com:27489/heroku_app21826196",
     app: {
         name: "MEAN - A Modern Stack - Production"
     },
